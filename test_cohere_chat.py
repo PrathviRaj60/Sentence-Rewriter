2220d3209afb91b8941049101451a755bd3b4d68
Prathviraj <121480537+PrathviRaj60@users.noreply.github.com>
2025-06-06 21:35:50 +0530

    Removed a useless file

--- test_cohere_chat.py
+++ test_cohere_chat.py
@@ -1,13 +1,1 @@
-import cohere
 
-API_KEY = "TgyrJfpx02UujwLyhe7qodbRR0NSsSTLMlBWCZvo"
-client = cohere.Client(API_KEY)
-
-response = client.chat(
-    model="command-xlarge-nightly",
-    messages=[
-        {"role": "user", "content": "Rewrite this sentence in a casual tone."}
-    ]
-)
-
-print(response.choices[0].message.content)
